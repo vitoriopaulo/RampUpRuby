ce394898c1efeaf4b84bb02219e3addd5aa6ab06
Vitorio Paulo <vitoriopaulo@users.noreply.github.com>
2015-08-12 23:34:03 -0400

    Update Yob.rb
    Some adjustments where made in the code, in order to make the program even more user-friendly. This program is currently in Beta-Status, and needs adjustments in the part of the code that data process the year of birth (please kindly check line 50) in order to be fully functional. Such adjustments will be troubleshooted asap.

--- Yob.rb
+++ Yob.rb
@@ -1,22 +1,27 @@
-#StartUp Institute Ruby RampUp 2015.2
+#StartUp Institute Ruby RampUp 2015
 
 #Mentor: Samuel Backus
 
-#Program name: yob.rb
+#Author: Vitorio Paulo
 
-#Path: C/Documents/STARTUPRUBY/EXERC/EXERC WEEK01
+#Program name: Yob.rb
 
-#Code Author: Vitorio Paulo
+#Path: Favorites/Vitorio/rubys
+
+#Last Update: 08.12.2015, Wednesday.
 
 puts "\n"
 
-puts "Hi! Hope you're doing good!"
+puts "Hello, hope you're doing good. :)"
 puts "\n"
 
 puts "This program will tell you the year you were born."
 puts "\n"
 
 puts "You will be asked some questions. Please kindly answer then as follow."
+puts "\n"
+
+puts "Note: press Enter after texting the answers! :)"
 puts "\n"
 
 puts "First question: What's your name?"
@@ -29,7 +34,7 @@ puts "\n"
 puts "Let's keep moving."
 puts "\n"
 
-puts "Second question: How old are you? (Answer only with numbers. Ex: 17, 117, etc)"
+puts "Second question: How old are you? (Please prompt the answer only with whole numbers. Ex: 17, 117, etc)"
 age = gets.chomp
 puts "\n"
 
