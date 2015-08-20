52a333416691f51d8ed49cea3b19b1454733f5a9
Vitorio Paulo <vitoriopaulo@users.noreply.github.com>
2015-08-20 11:21:54 -0400

    Create Madlib.rb
    A madlib ruby game. :)

---
+++ Madlib.rb
@@ -0,0 +1,108 @@
+#StartUp Institute Ruby RampUp 2015.2
+
+#Mentor: Samuel Backus
+
+#Author: Vitorio Paulo
+
+#Program name: Madlib.rb
+
+#Master branch: Favorites/Vitorio/rubys
+
+#Kick-off Date: August 20, 2015
+
+#Last Update: August 20, 2015
+
+puts "\n"
+
+puts "Hi! Hope you're doing good."
+puts "\n"
+
+puts "This program will output a funny and quircky (though poetic) paragraph that, regardless of making no sense, has beauty."
+puts "\n"
+
+puts "You will be asked to prompt some questions. As such, just answer then as follow. No worries my dear! It will be fine. You'll have fun. :)"
+puts "\n"
+
+puts "NOTE 1: Press ENTER to confirm all your imputed data in all of the following questions."
+puts "\n"
+
+puts "Before starting to play, please kindly introduce yourself: What's your name? * Suggestion: type the first character of your name in Caps. :)"
+name = gets.chomp
+puts "\n"
+
+puts "Nice to meet you " + name + "!" + " Good job."
+puts "\n"
+
+puts "Let's keep moving."
+puts "\n"
+
+puts "NOTE 2: Please kindly answer all the following questions with the characters NOT capitalized."
+puts "\n"
+
+puts "1) Type an element of the solar system:"
+answer1 = gets.chomp
+puts "\n"
+
+puts "2) Prompt a month of the year:"
+answer2 = gets.chomp
+puts "\n"
+
+puts "3) Type a verb:"
+answer3 = gets.chomp
+puts "\n"
+
+puts "4) Type the name of a flower in its plural:"
+answer4 = gets.chomp
+puts "\n"
+
+puts "5) Prompt a color:"
+answer5 = gets.chomp
+puts "\n"
+
+puts "6) Prompt a mean of transportation in its plural:"
+answer6 = gets.chomp
+puts "\n"
+
+puts "7) Type another month of the year:"
+answer7 = gets.chomp
+puts "\n"
+
+puts "8) Choose: Dream or Reality? If Dream, prompt Dream; Otherwise Reality, type Reality:"
+answer8 = gets.chomp
+puts "\n"
+
+puts "9) Type another color:"
+answer9 = gets.chomp
+puts "\n"
+
+puts "10) Prompt a flying animal in its singular:"
+answer10 = gets.chomp
+puts "\n"
+
+puts "Great job " + name + "!" + " All set! :)"
+puts "\n"
+
+puts "All your imputed data has been succesfully processed. Below follows the result of this Madlib game:"
+puts "\n"
+ 
+puts "The " + answer1 + " of " + answer2 + " " + answer3 + " " + answer4 + " and " + answer5 + " " + answer6 + " in the " + answer7 + " of your " + answer8 + " of " + answer9 + " " + answer10 + "."
+puts "\n"
+
+puts name + ", I have something to tell that might will upset you: game is over... :("
+puts "\n" 
+
+puts "But before saying bye, I have good news: you are more then welcome to play this Madlib game again whenever you want! :)"
+puts "\n"
+
+puts "Hope you had a lot of fun with us."
+puts "\n"
+
+puts "Take care and looking forward to see you again here soon " + name + "!" 
+puts "\n"
+
+puts "Bye!"
+puts "\n"
+
+puts ":)"
+puts "\n"
+
